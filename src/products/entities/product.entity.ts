679e498752219f7c728a49ad5126d288a060d20f
Julio Espinola <90486971+Julio9999@users.noreply.github.com>
2024-04-12 19:31:04 -0400

    seccion12

---
+++ src/products/entities/product.entity.ts
@@ -0,0 +1,82 @@
+import { BeforeInsert, BeforeUpdate, Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { ProductImage } from ".";
+
+@Entity({name: 'products'})
+export class Product {
+
+
+    private generateSlug(slug: string): string {
+        return slug
+        .toLowerCase()
+        .replaceAll(' ', '_')
+        .replaceAll("'", '');
+    }
+
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+
+    @Column('text', {
+        unique: true
+    })
+    title: string;
+
+    @Column('float', {
+        default: 0
+    })
+    price: number;
+
+    @Column({
+        type: 'text',
+        nullable: true
+    })
+    description: string;
+
+    @Column('text', {
+        unique: true
+    })
+    slug: string;
+
+    @Column('int', {
+        default: 0
+    })
+    stock: number;
+
+    @Column('text', {
+        array: true
+    })
+    sizes: string[]
+
+    @Column('text')
+    gender: string;
+
+
+    @Column('text', {
+        array: true,
+        default: []
+    })
+    tags: string[];
+
+    @OneToMany(
+        () => ProductImage,
+        (productImage) => productImage.product,
+        {cascade: true, eager: true}
+    )
+    images?: ProductImage[];
+
+
+    @BeforeInsert()
+    checkSlugInsert() {
+        if (!this.slug) {
+            this.slug = this.title
+        }
+
+        this.slug = this.generateSlug(this.slug)
+    }
+
+    @BeforeUpdate()
+    checkSlugUpdate() {
+        this.slug = this.generateSlug(this.slug)
+    }
+
+}
